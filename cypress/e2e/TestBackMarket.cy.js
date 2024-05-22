4df31daafc530703f337e599d3d27ee6df84e009
Amine-N <amine.naib0@gmail.com>
2024-05-22 12:27:27 +0200

    Cypress authentification

--- cypress/e2e/TestBackMarket.cy.js
+++ cypress/e2e/TestBackMarket.cy.js
@@ -2,8 +2,8 @@ describe('inscription', () => {
   it('Inscription KO', () => {
     cy.visit('https://preprod.backmarket.fr/register')
     cy.get('[data-qa="accept-cta"]').click()
-    cy.get('#firstName').type('Amine')
-    cy.get('#lastName').type('Naib')
+    cy.get('#firstName').type('Test')
+    cy.get('#lastName').type('Toto')
     cy.get('#signup-email').type('mail@gmail.com')
     cy.get('#signup-password').type('Motdepasse')
     cy.get('[data-qa="signup-submit-button"] > .MkLAMntR > ._2GvJDBxS').click()
@@ -12,11 +12,18 @@ describe('inscription', () => {
   it('Inscription OK', () => {
     cy.visit('https://preprod.backmarket.fr/register')
     cy.get('[data-qa="accept-cta"]').click()
-    cy.get('#firstName').type('Amine')
-    cy.get('#lastName').type('Naib')
+    cy.get('#firstName').type('Test')
+    cy.get('#lastName').type('Toto')
     cy.get('#signup-email').type('mailtest02440@gmail.com')
     cy.get('#signup-password').type('Motdepasse0')
     cy.get('[data-qa="signup-submit-button"] > .MkLAMntR > ._2GvJDBxS').click()
   })
-})
 
+  it('Connexion', () => {
+    cy.visit('https://preprod.backmarket.fr/register')
+    cy.get('[data-qa="accept-cta"]').click()
+    cy.get('#signin-email').type('mailtest02440@gmail.com')
+    cy.get('#signin-password').type('Motdepasse0')
+    cy.get('[data-qa="signin-submit-button"] > .MkLAMntR > ._2GvJDBxS').click()
+  })
+})
