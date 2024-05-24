529bffbb2c656981b475159dd5554066acecb623
Amine-N <amine.naib0@gmail.com>
2024-05-24 16:47:18 +0200

    TestMagento terminé

--- cypress/e2e/TestMagento.cy.js
+++ cypress/e2e/TestMagento.cy.js
@@ -8,9 +8,32 @@ describe('Magento', () => {
     cy.get('#product-addtocart-button')
       .click()
       .should("be.visible", "You added Driven Backpack to your shopping cart.")
-    cy.wait(10000)
+    cy.wait(3000)
     cy.get('.counter-number').click()
     cy.get('#top-cart-btn-checkout').click()
+    cy.wait(6000)
+
+    cy.get('#customer-email').type('zazo@gmail.com')
+    cy.get('.input-text[name="firstname"]').type('Bernie')
+    cy.get('.input-text[name="lastname"]').type('Maxi')
+    cy.get('input[name="street[0]"]').type('22 rue pont michel cost')
+    cy.get('.input-text[name="city"]').type('Bordeaux')
+    cy.get('select[name="country_id"]').select('France')
+    cy.get('select[name="region_id"]').select('Gironde')
+    cy.get('.input-text[name="postcode"]').type('33100')
+    cy.get('.input-text[name="telephone"]').type('0600000000')
+    cy.get('input[type="radio"][value="tablerate_bestway"]').check()
+    cy.wait(1000)
+
+    cy.get('.button > span').click()
+    cy.get('button[data-role="opc-continue"]').click()
+    cy.contains('button', 'Next').click()
+    cy.get('button.button.action.continue.primary').click()
+    cy.wait(5000)
+
+    cy.contains('span', 'Place Order').click()
+    cy.contains('Thank you for your purchase!').should("be.visible")
+    
 
   })
 })
